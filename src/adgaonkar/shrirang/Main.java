af9aaa878b5846c89655c1b19d0d5347642dfb1f
shrirang91 <sadgaonk@asu.edu>
2016-03-30 23:07:17 -0700

    Reading from files

--- src/adgaonkar/shrirang/Main.java
+++ src/adgaonkar/shrirang/Main.java
@@ -1,82 +1,109 @@
 package adgaonkar.shrirang;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
-import java.math.*;
-class Main{
-	public static void main(String[] args){
-		
-		Scanner s=new Scanner(System.in);
-		System.out.println("enter no of players: ");
-		int n=s.nextInt();
-		int days;
-		int [] list;
-		//check whether # of players is even or odd to SET # OF DAYS REQD
-		if(n%2==0){
-			days=n-1;
-			list=new int[n];
-			for(int i=0;i<n;i++)
-				list[i]=i+1;
-		}
-		else{
-			days=n;
-			list=new int[n+1];
-			for(int i=0;i<n;i++)
-				list[i]=i+1;
-			list[n]=n+1;
-		}
-		
-		int size=list.length;
-		System.out.println("size is :"+size);
-		int [] listA=new int[size/2];
-		int [] listB=new int[size/2];
-		
-		// now split array into 2 halves. and assign values
-			for(int i=0;i<size/2;i++)
-				listA[i]=list[i];
-			for(int i=0;i<size/2;i++)
-				listB[i]=list[size-i-1];
-		
-			
-			int [] listP=new int[size];	
-		//print	
-			int k=0;
-		while(days>0){
-			days--;k++;
-			for(int i=0;i<size/2;i++){
-				listP[(listA[i]-1)]=listB[i];
-				listP[(listB[i]-1)]=listA[i];
+
+class Main {
+	public static void scheduler(int n) {
+		if (n <= 1)
+			System.out.print("\ncannot create a schedule");
+		else if (n == 2)
+			System.out.println("1:2:1");
+		else {
+			int days;
+			int[] list;
+			// check whether # of players is even or odd to SET # OF DAYS REQD
+			if (n % 2 == 0) {
+				days = n - 1;
+				list = new int[n];
+				for (int i = 0; i < n; i++)
+					list[i] = i + 1;
+			} else {
+				days = n;
+				list = new int[n + 1];
+				for (int i = 0; i < n; i++)
+					list[i] = i + 1;
+				list[n] = n + 1;
 			}
-			
-			/*System.out.println("------listP\n");
-			for(int i=0;i<size;i++)
-				System.out.print(listP[i]+"\t");
-			System.out.println("------P\n");
-			*/
-			
-			
-			System.out.print("\n"+k);
-			if(n%2==0){
-			for(int i=0;i<size;i++)	
-				System.out.print(":"+listP[i]);
-			}
-			else{
-				for(int i=0;i<size-1;i++){
-					if(listP[i]==n+1)
-						System.out.print(":-");
-					else if(listP[i]==(i+1))
-						System.out.print(":-");
-					else
-						System.out.print(":"+listP[i]);
+
+			int size = list.length;
+
+			// System.out.println("size is :"+size);
+			int[] listA = new int[size / 2];
+			int[] listB = new int[size / 2];
+
+			// now split array into 2 halves. and assign values
+			for (int i = 0; i < size / 2; i++)
+				listA[i] = list[i];
+			for (int i = 0; i < size / 2; i++)
+				listB[i] = list[size - i - 1];
+
+			int[] listP = new int[size];
+			// print
+			int k = 0;
+			while (days > 0) {
+				days--;
+				k++;
+				for (int i = 0; i < size / 2; i++) {
+					listP[(listA[i] - 1)] = listB[i];
+					listP[(listB[i] - 1)] = listA[i];
 				}
+
+				System.out.print("\n" + k);
+				if (n % 2 == 0) {
+					for (int i = 0; i < size; i++)
+						System.out.print(":" + listP[i]);
+				} else {
+					for (int i = 0; i < size - 1; i++) {
+						if (listP[i] == n + 1)
+							System.out.print(":-");
+						else if (listP[i] == (i + 1))
+							System.out.print(":-");
+						else
+							System.out.print(":" + listP[i]);
+					}
+				}
+
+				int tempA = listA[size / 2 - 1];
+				int tempB = listB[0];
+				for (int i = size / 2; i > 1; i--) {
+					listA[i - 1] = listA[i - 2];
+					listB[size / 2 - i] = listB[size / 2 - i + 1];
+				}
+				listA[1] = tempB;
+				listB[size / 2 - 1] = tempA;
 			}
-			
-			int tempA=listA[size/2-1];
-			int tempB=listB[0];
-			for(int i=size/2;i>1;i--){
-				listA[i-1]=listA[i-2];
-				listB[size/2-i]=listB[size/2-i+1];
-			}
-			listA[1]=tempB;
-			listB[size/2-1]=tempA;
 		}
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException{
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("enter 1 to use console input");
+		System.out.println("enter 2 to use file input");
+		int choice = sc.nextInt();
+		if (choice == 1) {
+			int c;
+			do{
+			System.out.println("enter no of players: ");
+			int n = sc.nextInt();
+			scheduler(n);
+			System.out.println("\nEnter 0 to exit or a positive number to restart scheduler");
+			c=sc.nextInt();
+			}while(c!=0);
+		}
+		else if(choice == 2){
+			int np;
+			Scanner read= new Scanner(new File(System.getProperty("user.dir") + "/input.txt"));
+			while(read.hasNextInt())
+			{
+				np= read.nextInt();
+				scheduler(np);
+			}
+		}		
 	}
 }
